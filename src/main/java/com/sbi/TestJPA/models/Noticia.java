a51afcbeda34a89b7b2e35bcc5c05cff187bc01e
Edwin Agudelo <117861779+EA-Quorum@users.noreply.github.com>
2024-06-26 18:06:29 -0500

    Upgraded to Spring 3 (#5)

--- src/main/java/com/sbi/TestJPA/models/Noticia.java
+++ src/main/java/com/sbi/TestJPA/models/Noticia.java
@@ -1,13 +1,13 @@
 package com.sbi.TestJPA.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.JoinColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 public class Noticia {
